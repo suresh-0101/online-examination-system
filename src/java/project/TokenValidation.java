7fb7f6205a7a59e408338873226d7a9a237aaba5
suresh <sureshm.star369@gmail.com>
2023-10-15 15:10:04 +0530

    new constructor add

--- src/java/project/TokenValidation.java
+++ src/java/project/TokenValidation.java
@@ -1,9 +1,12 @@
 package project;
 
 public class TokenValidation {
-
+ private int number;
     public TokenValidation(){
 
+    }
+    public TokenValidation(int n){
+        this.number = n;
     }
 
 }
